acda8e26c773a38e61f747bc41bbf9a93045710d
José Semedo <jose.francisco.semedo@gmail.com>
2015-10-25 21:03:34 +0000

    changed the table so the road has different color, and added a finish/start line

--- table.cpp
+++ table.cpp
@@ -3,22 +3,36 @@
 Table::Table() {}
 Table::~Table() {}
 GLvoid Table::draw(){
-	
-    
     //start line
     glPushMatrix();
     glColor3f(1, 0, 0);
-    glTranslatef(ROAD_OUT_RADIUS - ROAD_WIDTH/2, 0, -0.4);
+    glTranslatef(ROAD_OUT_RADIUS - ROAD_WIDTH/2, 0, -0.39);
     glScalef(ROAD_WIDTH - (CHEERIO_RADIUS + CHEERIO_RADIUS_TUBE), 2, 1);
     glutSolidCube(1);
     glPopMatrix();
     
     //table
-    glColor3f(0.528, 0.528, 0.528); // glColor3f(0.7, 0.7, 0.7);
-	glPushMatrix();
-	glTranslatef( 0.0f, 0.0f, -20.);
+    glColor3f(0.7, 0.7, 0.7); // glColor3f(0.7, 0.7, 0.7);
+    glPushMatrix();
+    glTranslatef( 0.0f, 0.0f, -20.);
     
     glScalef(1, 1, 40./TABLE_SIZE);
-	glutSolidCube(TABLE_SIZE);
+    glutSolidCube(TABLE_SIZE);
+    glPopMatrix();
+    
+    //inner table circle
+    glPushMatrix();
+    glColor3f(0.7, 0.7, 0.7); // glColor3f(0.7, 0.7, 0.7);
+    glTranslatef(0.0, 0.0, 0.11);
+    glRotatef(90, 0, 1, 0);
+    glutSolidSphere(ROAD_OUT_RADIUS - ROAD_WIDTH, 2, 16);
+    glPopMatrix();
+    
+    //outter table ring
+    glPushMatrix();
+    glColor3f(0.528, 0.528, 0.528); // glColor3f(0.7, 0.7, 0.7);
+    glTranslatef(0.0, 0.0, 0.1);
+    glRotatef(90, 0, 1, 0);
+    glutSolidSphere(ROAD_OUT_RADIUS, 2, 16);
     glPopMatrix();
 }
