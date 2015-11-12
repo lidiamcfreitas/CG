ac9f6ba6477701d6b48a2cd09fa902b28804feac
Lídia Freitas <lidiamcfreitas@gmail.com>
2015-11-12 20:39:58 +0000

    removed prints and moved old commented stuff

--- car.cpp
+++ car.cpp
@@ -56,12 +56,6 @@ GLvoid Car::draw(){
     GLfloat spec[]={0.633f,0.727811f,0.633f,1.0f};
     GLfloat shine=76.8f;
     
-    /* RUBBER WHEELS
-    GLfloat amb_wheel[]={0.02f,0.02f,0.02f,1.0f};
-    GLfloat diff_wheel[]={0.01f,0.01f,0.01f,1.0f};
-    GLfloat spec_wheel[]={0.4f,0.4f,0.4f,1.0f};
-    GLfloat shine_wheel=10.0f;
-     */
     // SHINY WHEELS
     GLfloat amb_1[]={0.0f,0.0f,0.0f,1.0f};
     GLfloat diff_1[]={0.11f,0.11f,0.11f,1.0f};
@@ -158,7 +152,6 @@ GLvoid Car::draw(){
     
     //wheels
     glPushMatrix();
-    //material(amb_wheel, diff_wheel, spec_wheel, shine_wheel);
     material(amb_1, diff_1, spec_1, shine_1);
     glColor3f(0.26, 0.15, 0.2);//dark grey
     
@@ -171,108 +164,6 @@ GLvoid Car::draw(){
     glPopMatrix();
     
     glPopMatrix();
-    
-    /* CARRO FOFO!
-     
-     GLfloat amb_red[]={0.1745f,0.01175f,0.01175f,1.0f};
-     GLfloat diff_red[]={0.61424f,0.04136f,0.04136f,1.0f};
-     GLfloat spec_red[]={0.727811f,0.626959f,0.626959f,1.0f};
-     GLfloat shine_red=76.8f;
-     
-     GLfloat amb_lights[]={0.05f,0.05f,0.0f,1.0f};
-     GLfloat diff_lights[]={0.5f,0.5f,0.4f,1.0f};
-     GLfloat spec_lights[]={0.7f,0.7f,0.04f,1.0f};
-     GLfloat shine_lights=10.0f;
-     
-    //---------------------------------------main body
-    glColor3f(0.0, 0.5, 0.0);//dark green
-	glPushMatrix();
-    material(amb, diff, spec, shine);
-    glTranslatef(0.0, 0.0, 3.0);
-    glScalef(1.0f, 1.2f, 0.7f);
-    
-    if(solidOrWire)
-        glutSolidCube(5);
-    else
-        glutWireCube(5);
-    
-	glPopMatrix();
-
-    //---------------------------------------front body
-    glColor3f(0.0, 0.5, 0.0);//dark green
-	glPushMatrix();
-    material(amb, diff, spec, shine);
-    glTranslatef(0.0, 4.0, 1.3f);
-    glScalef(1.5f, 1.0f, 1.0f);
-    if(solidOrWire) {
-        glutSolidCube(2);
-    }
-    else{
-        glutWireCube(2);
-    }
-	glPopMatrix();
-
-    //---------------------------------------back body
-    glColor3f(0.0,  0.5, 0.0);//dark green
-	glPushMatrix();
-    material(amb, diff, spec, shine);
-    glTranslatef(0.0, 3.5, 2.1);
-    glScalef(3.0f, 1.0f, 3.5f);
-    if(solidOrWire){
-        glutSolidCube(1);
-    }
-    else{
-        glutWireCube(1);
-    }
-	glPopMatrix();
-
-    //--------------------------------------front axis
-    glColor3f(0.7, 0.0, 0.0);//red
-    glPushMatrix();
-    material(amb_red, diff_red, spec_red, shine_red);
-    glTranslatef(0.0, 4.5, 1.5f);
-    glScalef(26.0f, 1.0f, 1.0f);
-    if(solidOrWire){
-        glutSolidCube(0.25);
-    }
-    else{
-        glutWireCube(0.25);
-    }
-    glPopMatrix();
-    
-    //--------------------------------------lights
-    glColor3f(1, 1, 0.0);//yellow
-    glPushMatrix();
-    material(amb_lights, diff_lights, spec_lights, shine_lights);
-    glTranslatef(0.0, 5, 1.5f);
-    GLfloat emiss[]={_lights_mat_emiss.getX(),_lights_mat_emiss.getY(),_lights_mat_emiss.getZ(),_lights_mat_emiss.getW()};
-    glMaterialfv(GL_FRONT, GL_EMISSION, emiss);
-    glPushMatrix();
-    glTranslatef(1, 0, 0);
-    if(solidOrWire)
-        glutSolidCube(1);
-    else
-        glutWireCube(1);
-    glPopMatrix();
-    
-    glPushMatrix();
-    glTranslatef(-1, 0, 0);
-    if(solidOrWire)
-        glutSolidCube(1);
-    else
-        glutWireCube(1);
-    glPopMatrix();
-    
-    glPopMatrix();
-    
-    drawWheel(2.7, 4.5, 1.6, 0.6, 1.0, solidOrWire);//front right wheel
-    drawWheel(-2.7, 4.5, 1.6, 0.6, 1.0, solidOrWire);//front left wheel
-    drawWheel(3.0, -3.0, 3.2, 1.2, 2.0, solidOrWire);//back right wheel
-    drawWheel(-3.0, -3.0, 3.2, 1.2, 2.0, solidOrWire);//back left wheel
-
-    glPopMatrix();
-    
-    glPopMatrix();*/
 }
 
 GLvoid Car::drawWheel(GLfloat wheelx, GLfloat wheely, GLfloat wheelz, GLfloat wheelInner, GLfloat wheelOuter, GLboolean solidOrWire){
@@ -354,7 +245,6 @@ GLvoid Car::update(GLdouble delta_t){
 }
 
 GLvoid Car::drawWheel(GLfloat wheelWidth, GLfloat radius, GLfloat x, GLfloat y){
-    printf("init drawing");
     glPushMatrix();
     glTranslatef(x, y, radius);
     glBegin(GL_TRIANGLE_STRIP);
@@ -366,9 +256,10 @@ GLvoid Car::drawWheel(GLfloat wheelWidth, GLfloat radius, GLfloat x, GLfloat y){
     std::vector<Vector3> rightNormals;
     Vector3 vector;
     Vector3 normal;
+    
+    // SIDE STRIP
     for (GLint i=0; i<degree+1; i++) {
         GLfloat angle = (2*M_PI/degree)*i;
-        //printf("%f\n", angle);
         
         vector.set(wheelWidth/2, radius * cosf(angle), radius * sinf(angle));
         normal.set(vector.getX()/vector.norm(),vector.getY()/vector.norm(),vector.getZ()/vector.norm());
@@ -405,3 +296,116 @@ GLvoid Car::drawWheel(GLfloat wheelWidth, GLfloat radius, GLfloat x, GLfloat y){
     glEnd();
     glPopMatrix();
 }
+
+
+// OTHER STUFF
+
+/* RUBBER WHEELS
+ GLfloat amb_wheel[]={0.02f,0.02f,0.02f,1.0f};
+ GLfloat diff_wheel[]={0.01f,0.01f,0.01f,1.0f};
+ GLfloat spec_wheel[]={0.4f,0.4f,0.4f,1.0f};
+ GLfloat shine_wheel=10.0f;
+ */
+
+//CARRO FOFO!
+/*
+ GLfloat amb_red[]={0.1745f,0.01175f,0.01175f,1.0f};
+ GLfloat diff_red[]={0.61424f,0.04136f,0.04136f,1.0f};
+ GLfloat spec_red[]={0.727811f,0.626959f,0.626959f,1.0f};
+ GLfloat shine_red=76.8f;
+ 
+ GLfloat amb_lights[]={0.05f,0.05f,0.0f,1.0f};
+ GLfloat diff_lights[]={0.5f,0.5f,0.4f,1.0f};
+ GLfloat spec_lights[]={0.7f,0.7f,0.04f,1.0f};
+ GLfloat shine_lights=10.0f;
+ 
+ //---------------------------------------main body
+ glColor3f(0.0, 0.5, 0.0);//dark green
+	glPushMatrix();
+ material(amb, diff, spec, shine);
+ glTranslatef(0.0, 0.0, 3.0);
+ glScalef(1.0f, 1.2f, 0.7f);
+ 
+ if(solidOrWire)
+ glutSolidCube(5);
+ else
+ glutWireCube(5);
+ 
+	glPopMatrix();
+ 
+ //---------------------------------------front body
+ glColor3f(0.0, 0.5, 0.0);//dark green
+	glPushMatrix();
+ material(amb, diff, spec, shine);
+ glTranslatef(0.0, 4.0, 1.3f);
+ glScalef(1.5f, 1.0f, 1.0f);
+ if(solidOrWire) {
+ glutSolidCube(2);
+ }
+ else{
+ glutWireCube(2);
+ }
+	glPopMatrix();
+ 
+ //---------------------------------------back body
+ glColor3f(0.0,  0.5, 0.0);//dark green
+	glPushMatrix();
+ material(amb, diff, spec, shine);
+ glTranslatef(0.0, 3.5, 2.1);
+ glScalef(3.0f, 1.0f, 3.5f);
+ if(solidOrWire){
+ glutSolidCube(1);
+ }
+ else{
+ glutWireCube(1);
+ }
+	glPopMatrix();
+ 
+ //--------------------------------------front axis
+ glColor3f(0.7, 0.0, 0.0);//red
+ glPushMatrix();
+ material(amb_red, diff_red, spec_red, shine_red);
+ glTranslatef(0.0, 4.5, 1.5f);
+ glScalef(26.0f, 1.0f, 1.0f);
+ if(solidOrWire){
+ glutSolidCube(0.25);
+ }
+ else{
+ glutWireCube(0.25);
+ }
+ glPopMatrix();
+ 
+ //--------------------------------------lights
+ glColor3f(1, 1, 0.0);//yellow
+ glPushMatrix();
+ material(amb_lights, diff_lights, spec_lights, shine_lights);
+ glTranslatef(0.0, 5, 1.5f);
+ GLfloat emiss[]={_lights_mat_emiss.getX(),_lights_mat_emiss.getY(),_lights_mat_emiss.getZ(),_lights_mat_emiss.getW()};
+ glMaterialfv(GL_FRONT, GL_EMISSION, emiss);
+ glPushMatrix();
+ glTranslatef(1, 0, 0);
+ if(solidOrWire)
+ glutSolidCube(1);
+ else
+ glutWireCube(1);
+ glPopMatrix();
+ 
+ glPushMatrix();
+ glTranslatef(-1, 0, 0);
+ if(solidOrWire)
+ glutSolidCube(1);
+ else
+ glutWireCube(1);
+ glPopMatrix();
+ 
+ glPopMatrix();
+ 
+ drawWheel(2.7, 4.5, 1.6, 0.6, 1.0, solidOrWire);//front right wheel
+ drawWheel(-2.7, 4.5, 1.6, 0.6, 1.0, solidOrWire);//front left wheel
+ drawWheel(3.0, -3.0, 3.2, 1.2, 2.0, solidOrWire);//back right wheel
+ drawWheel(-3.0, -3.0, 3.2, 1.2, 2.0, solidOrWire);//back left wheel
+ 
+ glPopMatrix();
+ 
+ glPopMatrix();*/
+
